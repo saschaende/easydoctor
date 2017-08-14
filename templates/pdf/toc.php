edfe30e6882affed29c96b018f2eb7c34aa00215
Sascha Ende <s.ende@pixelcreation.de>
2017-08-14 14:30:32 +0200

    [bugfix] toc numbers for h1

--- templates/pdf/toc.php
+++ templates/pdf/toc.php
@@ -5,7 +5,7 @@
         <?php $h1count=1; $h2count=1; foreach ($toc as $line): ?>
             <tr>
                 <?php if ($line['type'] == 'h1'): ?>
-                    <td style="width:100%;"><a href="#page<?php echo $h1count; ?>" style="font-weight: bold;"><?php echo $i; ?>. <?php echo $line['title']; ?></a></span></td>
+                    <td style="width:100%;"><a href="#page<?php echo $h1count; ?>" style="font-weight: bold;"><?php echo $h1count; ?>. <?php echo $line['title']; ?></a></span></td>
                     <?php $h1count++; ?>
                 <?php endif; ?>
                 <?php if ($line['type'] == 'h2'): ?>
